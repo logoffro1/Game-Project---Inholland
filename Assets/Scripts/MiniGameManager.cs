85494e3e72d51129f69d74d9fae43bb42f1580f8
Cosmin Ilie <cosmin.ilie2704@gmail.com>
2022-03-15 14:21:14 +0100

    created mini game prefab / changed MiniGameManager

--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -7,8 +7,7 @@ public class MiniGameManager : MonoBehaviour
     private static MiniGameManager _instance = null;
     public static MiniGameManager Instance { get { return _instance; } }
     public bool IsPlaying { get; private set; }
-    public GameObject SewageGamePrefab;
-    private GameObject SewageMiniGame;
+    private GameObject miniGame;
     public GameObject miniGameScreen;
     private void Awake()
     {
@@ -31,18 +30,17 @@ public class MiniGameManager : MonoBehaviour
     }
     public void StartGame(GameObject miniGamePrefab)
     {
-        SewageMiniGame = Instantiate(miniGamePrefab, new Vector3(0, 0, 100), SewageGamePrefab.transform.rotation);
-        miniGameScreen.SetActive(true);
-        IsPlaying = true;
+        if (IsPlaying) return;
+
         UIManager.Instance.ChangeCanvasShown();
+        miniGame = Instantiate(miniGamePrefab, new Vector3(0, 0, 100), miniGamePrefab.transform.rotation);
+        IsPlaying = true;     
     }
-    private IEnumerator StopGame()
+    public IEnumerator StopGame()
     {
         yield return new WaitForSeconds(.5f);
-        Destroy(SewageMiniGame);
-        miniGameScreen.SetActive(false);
+        Destroy(miniGame);
         IsPlaying = false;
-
         UIManager.Instance.ChangeCanvasShown();
     }
 }
