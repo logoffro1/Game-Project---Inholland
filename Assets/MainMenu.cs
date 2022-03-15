6f813eed7214769f6d9b165eb16e57a13ea79635
Dewi <dewi.becu@gmail.com>
2022-03-15 23:11:05 +0100

    Added all necessary components of main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void ContinueGame()
     {
+        //TODO: Get save file, load safe file
+        //Load lobby scene
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void NewGame()
+    {
+        //TODO: If save file exists, ask if it can be deleted (no --> go back to main menu)
+        // Yes --> create new save file, start tutorial
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
