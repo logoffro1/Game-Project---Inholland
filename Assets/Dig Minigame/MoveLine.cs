a3e94275006dc47cc1adf02105871b5544112aa2
omaralawin <omaralawin@gmail.com>
2022-03-19 19:32:32 +0100

    added base functionality of dig minigame

--- Assets/Dig Minigame/MoveLine.cs
+++ Assets/Dig Minigame/MoveLine.cs
@@ -5,29 +5,54 @@ using UnityEngine;
 public class MoveLine : MonoBehaviour
 {
     bool gameOver;
+    bool movingRight = false;
+    double leftLimitX = -0.495f;
+    double rightLimitX = 0.495f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameOver == true)
+        if (movingRight == false)
         {
-            return;
+            transform.Translate(Vector2.left * 2f * Time.deltaTime);
+            if (transform.localPosition.x <= leftLimitX)
+            {
+                movingRight = true;
+                Debug.Log(movingRight);
+            }
         }
-        //moves it to the left, setup up boundaries
-        transform.Translate(Vector2.left * 2f * Time.deltaTime);
+        else
+        {
+            transform.Translate(Vector2.right * 2f * Time.deltaTime);
+            if (transform.localPosition.x >= rightLimitX)
+            {
+                movingRight = false;
+            }
+        }
+
+
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+   private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Enter");
     }
 
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Exit");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Victory!");
+        }
     }
 }
--- Assets/Dig Minigame/SpawnTarget.cs
+++ Assets/Dig Minigame/SpawnTarget.cs
@@ -7,12 +7,14 @@ public class SpawnTarget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float rnd = Random.Range(-0.45f,0.45f);
+        Debug.Log(rnd);
+        transform.position = new Vector3(69,69,69);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
