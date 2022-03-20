37db2e753b092ee6c0cf6554d246f3ae88de7ba0
Dewi <dewi.becu@gmail.com>
2022-03-20 22:25:12 +0100

    Used CompareTag and TryGetComponent in Wire class

--- Assets/MiniGames/RewireMiniGame/Scripts/Wire.cs
+++ Assets/MiniGames/RewireMiniGame/Scripts/Wire.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Wire : MonoBehaviour
 {
-    /* Notes from Cosmin
-     * 1. Don't make variables public unless there is no other option but to assign from inspector
-     *    If you want to see variables in the inspector for debugging, add [SerializeField] in front of a private field and it will show in the inspector
-     *    Or for stuff that you want to use outside the class, use properties (get/ private set)
-     * 2. If you're gonna use tags, use .CompareTag() instead of '==' to compare them, it's faster and less prone to errors
-     * 3. Look into TryGetComponent<> - https://docs.unity3d.com/ScriptReference/Component.TryGetComponent.html
-     * 4. you can use an array to store the Presets (or even dictionary if you wanna assign them a name), but that's mostly just so it looks prettier 
-     */
-
     public Color color;
 
     private int darkMultiplier = 3;
@@ -24,46 +15,46 @@ public class Wire : MonoBehaviour
     {
         foreach(Transform child in transform)
         {
-            var childSprite = child.GetComponent<SpriteRenderer>();
-
-            Color tmpColor = color;
-
-            if (childSprite != null)
+            if (child.TryGetComponent(out SpriteRenderer childSprite))
             {
-                childSprite.color = color;
+                Color tmpColor = color;
 
-                if (childSprite.tag == "WireBackgroundPoint")
+                if (childSprite != null)
                 {
-                    tmpColor = (tmpColor / darkMultiplier) + darkPreset;
-                }
-                else if (childSprite.tag == "WireTop")
-                {
-                    tmpColor = tmpColor + mediumPreset;
-                }
+                    childSprite.color = color;
 
-                tmpColor.a = 1;
-                childSprite.color = tmpColor;
-
-            }
-            else
-            {
-                foreach (Transform child2 in child)
-                {
-                    tmpColor = color;
-
-                    var childSprite2 = child2.GetComponent<SpriteRenderer>();
-
-                    if (childSprite2.tag == "WireEnd")
+                    if (childSprite.CompareTag("WireBackgroundPoint"))
+                    {
+                        tmpColor = (tmpColor / darkMultiplier) + darkPreset;
+                    }
+                    else if (childSprite.CompareTag("WireTop"))
                     {
                         tmpColor = tmpColor + mediumPreset;
                     }
 
                     tmpColor.a = 1;
-                    childSprite2.color = tmpColor;
+                    childSprite.color = tmpColor;
 
+                }
+            }
+            else
+            {
+                foreach (Transform childOfChild in child)
+                {
+                    Color tmpColor = color;
+
+                    if (childOfChild.TryGetComponent(out SpriteRenderer childSpriteOfChildSprite))
+                    {
+                        if (childSpriteOfChildSprite.CompareTag("WireEnd"))
+                        {
+                            tmpColor = tmpColor + mediumPreset;
+                        }
+
+                        tmpColor.a = 1;
+                        childSpriteOfChildSprite.color = tmpColor;
+                    }
                 }
             }
         }
     }
-
 }
