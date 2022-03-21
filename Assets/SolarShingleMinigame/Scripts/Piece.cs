6108c1444cdad7bb21d373b59558c3540b6f19c6
egecnr <54850459+egecnr@users.noreply.github.com>
2022-03-21 18:58:46 +0100

    Taking a break.

---
+++ Assets/SolarShingleMinigame/Scripts/Piece.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Board board { get; private set; }
+    public TetrominoData tetrisData { get; private set; }
+    public Vector3Int position { get; private set; }
+    public void Init(Board board,Vector3Int position, TetrominoData tetrisData)
+    {
+        this.board = board;
+        this.position = position;
+        this.tetrisData = tetrisData;
+    }
+}
