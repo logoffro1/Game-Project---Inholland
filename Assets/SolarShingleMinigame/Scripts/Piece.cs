1b541757c5469a77afab9e670faab15d81492fff
egecnr <54850459+egecnr@users.noreply.github.com>
2022-03-22 00:37:57 +0100

    Implemented changes and added new sprites made by dewdew

--- Assets/SolarShingleMinigame/Scripts/Piece.cs
+++ Assets/SolarShingleMinigame/Scripts/Piece.cs
@@ -7,6 +7,8 @@ public class Piece : MonoBehaviour
     public Board board { get; private set; }
     public TetrominoData tetrisData { get; private set; }
     public Vector3Int position { get; private set; }
+
+    public Vector2Int[] cells { get; private set; }
     public void Init(Board board,Vector3Int position, TetrominoData tetrisData)
     {
         this.board = board;
