60b0ae39d0893517f286f29e338394f94f572aab
egecnr <54850459+egecnr@users.noreply.github.com>
2022-03-21 18:12:17 +0100

    Added tetris scripts and tetromino tiles

---
+++ Assets/SolarShingleMinigame/Scripts/Board.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public TetrominoData[] tetrominoes;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
