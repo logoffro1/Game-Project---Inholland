65a7a0959ffd2846bab644a70de1ba1d7daff4c5
egecnr <54850459+egecnr@users.noreply.github.com>
2022-03-22 07:15:52 +0100

    Fixed all the bugs now Im gonna take a break.

--- Assets/SolarShingleMinigame/Scripts/Board.cs
+++ Assets/SolarShingleMinigame/Scripts/Board.cs
@@ -3,16 +3,79 @@ using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
-    public TetrominoData[] tetrominoes;
     public Tilemap tilemap { get; private set; }
+
+    public Piece activePiece { get; private set; }
+    public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10,16);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2 ,-this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
         for(int i =0; i < this.tetrominoes.Length; i++)
         {
             this.tetrominoes[i].Init();
         }
+
+
+    }
+    public void SpawnPiece()
+    {
+        print(-this.boardSize.y / 2);
+
+        int random = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+        this.activePiece.Init(this, spawnPosition, data);
+        SetPiece(this.activePiece);
+        print("spawned");
+
+    }
+
+    public void SetPiece(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.tetrisData.tile);
+        }
+    }
+    public void ClearTile(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+        for(int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
     }
     void Start()
     {
@@ -22,17 +85,7 @@ public class Board : MonoBehaviour
     {
 
     }
-    public void SpawnPiece()
-    {
-        int random = Random.Range(0,tetrominoes.Length);
-        TetrominoData data = this.tetrominoes[random];
-    }
+  
 
-    public void SetTiles()
-    {
-
-    }
-
-    // Update is called once per frame
    
 }
--- Assets/SolarShingleMinigame/Scripts/Piece.cs
+++ Assets/SolarShingleMinigame/Scripts/Piece.cs
@@ -8,11 +8,68 @@ public class Piece : MonoBehaviour
     public TetrominoData tetrisData { get; private set; }
     public Vector3Int position { get; private set; }
 
-    public Vector2Int[] cells { get; private set; }
+    public Vector3Int[] cells { get; private set; }
     public void Init(Board board,Vector3Int position, TetrominoData tetrisData)
     {
         this.board = board;
         this.position = position;
         this.tetrisData = tetrisData;
+
+        if (this.cells == null)
+        {
+            this.cells = new Vector3Int[tetrisData.cells.Length];
+        }
+
+        for(int i = 0; i< tetrisData.cells.Length; i++)
+        {
+            this.cells[i] = (Vector3Int)tetrisData.cells[i];
+        }
+    }
+    private void Update()
+    {
+        this.board.ClearTile(this);
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K)){
+            Move(Vector2Int.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            HardDrop();
+        }
+
+        this.board.SetPiece(this);
+    }
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this,newPosition);
+
+        if (valid)
+        {
+            this.position = newPosition;
+        }
+        print("is it valid? " + valid);
+        return valid;
+        
     }
 }
