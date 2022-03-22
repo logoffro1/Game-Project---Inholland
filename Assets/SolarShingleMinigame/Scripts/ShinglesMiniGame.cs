d467c04941fb34b0fe3f79b4877bdbe3084f91d5
logoffro1 <cosmin.ilie2704@gmail.com>
2022-03-22 01:19:08 +0100

    installed shingles game

---
+++ Assets/SolarShingleMinigame/Scripts/ShinglesMiniGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinglesMiniGame : MiniGameBase
+{
+public void GameFinish(bool succesful)
+    {
+        if (succesful)
+            this.GameWon();
+        else
+            this.GameOver();
+    }
+}
