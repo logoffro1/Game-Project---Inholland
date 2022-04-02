16fd93ccbb67ee20957b4601fdd1dfb08e625bb1
egecnr <54850459+egecnr@users.noreply.github.com>
2022-04-02 04:42:44 +0200

    Added a piece of script to be able to delete icons

--- Assets/Scripts/HUD/MinimapScript.cs
+++ Assets/Scripts/HUD/MinimapScript.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class MinimapScript : MonoBehaviour
 {
     //Needs to be private after pitch
     private  List<InteractableTaskObject> allInteractableObjects;
+    public  List<GameObject> allPrefabLocations;
     public Transform player;
     public GameObject imagePrefab;
     public GameObject minimap;
@@ -15,9 +17,8 @@ public class MinimapScript : MonoBehaviour
     private void Start()
     {
         allInteractableObjects = new List<InteractableTaskObject>();
-        
+        allPrefabLocations = new List<GameObject>();
         initializeTaskList();
-
         PlantAllQuestIcons();
     }
     private void LateUpdate()
@@ -26,25 +27,58 @@ public class MinimapScript : MonoBehaviour
         Vector3 newPosition = player.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
-
-/*        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
-*/    }
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
 
     private void PlantAllQuestIcons()
     {
         foreach(InteractableTaskObject obj in allInteractableObjects)
         {
-            if(obj.Status!=TaskStatus.Success)
-            Instantiate(imagePrefab,new Vector3(obj.gameObject.transform.position.x, obj.gameObject.transform.position.y+20, obj.gameObject.transform.position.z),imagePrefab.transform.localRotation,minimap.transform);
+            Vector3 taskVector = new Vector3(obj.gameObject.transform.position.x, obj.gameObject.transform.position.y + 20, obj.gameObject.transform.position.z);
+            GameObject prefab = Instantiate(imagePrefab,taskVector,imagePrefab.transform.localRotation,minimap.transform);
+            allPrefabLocations.Add(prefab);
+
         }
     }
 
     private void initializeTaskList()
     {
-
         foreach (InteractableTaskObject interactableContainers in FindObjectsOfType<InteractableTaskObject>())
         {
             allInteractableObjects.Add(interactableContainers);
+        }
+    }
+
+    private GameObject GetPrefabByVector(Vector3 vector)
+    {
+        foreach(GameObject prefab in allPrefabLocations)
+        {
+            if(new Vector3(prefab.transform.position.x, prefab.transform.position.y, prefab.transform.position.z) == vector)
+            {
+                return prefab;
+            }
+        }
+        return null;
+    }
+
+    public  void DeleteIcon(Vector3 vector)
+    {
+        clearAllIcons();
+        allPrefabLocations.Remove(GetPrefabByVector(vector));
+        updateIcons();
+    }
+
+    private void clearAllIcons() { 
+        foreach(GameObject go in allPrefabLocations)
+        {
+            Destroy(go);
+        }   
+    }
+    private void updateIcons()
+    {
+        foreach (GameObject go in allPrefabLocations)
+        {
+            Instantiate(go);
         }
     }
 }
