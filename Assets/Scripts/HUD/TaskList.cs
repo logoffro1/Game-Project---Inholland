e5cfffc587e59b81b1b3021c14bf0b919782aef4
omaralawin <omaralawin@gmail.com>
2022-04-03 19:25:59 +0200

    Fixed Counter for total tasks being wrong

--- Assets/Scripts/HUD/TaskList.cs
+++ Assets/Scripts/HUD/TaskList.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TaskList : MonoBehaviour
 {
-    //InteractableTaskObject interactable = gameObject.GetComponent<InteractableTaskObject>();
     int solarCounter = 0;
-    int plantTreeCounter = 0;
-    int memoryGameCounter = 0;
+    int treeCounter = 0;
+    int lampCounter = 0;
     int sewerCounter = 0;
-    int wireCounter = 0;
     int totalObjects = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         FindObjects();
         Debug.Log("Shingles: "+ solarCounter);
         Debug.Log("Manhole: " + sewerCounter);
-        Debug.Log("Trees: " + plantTreeCounter);
-        Debug.Log("Street lamps: " + memoryGameCounter);
+        Debug.Log("Trees: " + treeCounter);
+        Debug.Log("Street lamps: " + lampCounter);
         Debug.Log("Total tasks: " + totalObjects);
     }
 
@@ -28,32 +28,41 @@ public class TaskList : MonoBehaviour
         
     }
 
-    List<InteractableTaskObject> FindObjects()
+    List<InteractableTaskStatusModels> FindObjects()
     {
-        List<InteractableTaskObject> list = new List<InteractableTaskObject>();
-
-        foreach (InteractableTaskObject task in FindObjectsOfType<InteractableTaskObject>())
-        {
-            if (task.tag == "SolarPanel")
+        List<InteractableTaskStatusModels> list = new List<InteractableTaskStatusModels>();
+        foreach (InteractableTaskStatusModels task in FindObjectsOfType<InteractableTaskStatusModels>().Where(x => x.gameObject.GetComponentInChildren<InteractableTaskObject>() && x.gameObject.GetComponentInChildren<InteractableTaskObject>().Status == TaskStatus.Untouched))
+        {           
+            if (task != null && task.enabled)
             {
-                solarCounter++;
+                if (task.tag == "SolarPanel")
+                {
+                    solarCounter++;
+                }
+                if (task.tag == "ManHole")
+                {
+                    sewerCounter++;
+                }
+                if (task.tag == "Tree")
+                {
+                    treeCounter++;
+                }
+                if (task.transform.CompareTag("StreetLamp"))
+                {
+                    lampCounter++;
+                }
+                Debug.Log(task.tag);
+                totalObjects++;
+                list.Add(task);
             }
-            if(task.tag == "ManHole")
-            {
-                sewerCounter++;
-            }
-            if (task.tag == "Tree")
-            {
-                plantTreeCounter++;
-            }
-            if (task.transform.tag == "StreetLamp")
-            {
-                memoryGameCounter++;
-            }
-            Debug.Log(task.tag);
-            totalObjects++;
-            list.Add(task);
         }
         return list;
+    }
+
+    void AssignTasks()
+    {
+        //pick 3 random tasks between shingles/manhole/trees/streetlamps, then assign a number between 1 and half the total of that task type to a player
+
+
     }
 }
