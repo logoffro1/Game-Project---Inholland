6895162dbe0c0940d2bbdc76f7579d2fab13aa08
logoffro1 <cosmin.ilie2704@gmail.com>
2022-04-11 10:18:17 +0200

    added battery draining

--- Assets/Scripts/XRayVision.cs
+++ Assets/Scripts/XRayVision.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 public class XRayVision : MonoBehaviour
 {
     [SerializeField] private RenderObjects normalRenderer;
     [SerializeField] private RenderObjects xrayRenderer;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private Image batteryFullImage;
     private Volume xrayVolume;
 
+    private bool drainOverTime = true;
+    [SerializeField] private float drainRate = 1f;
+    private float batteryLevel = 100f;
+
+    private bool flashing = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +34,62 @@ public class XRayVision : MonoBehaviour
         {
             ActivateVision();
         }
+        if (xrayVolume.enabled)
+        {
+            DrainBattery();
+        }
+
+    }
+    private void DrainBattery()
+    {
+        if (drainOverTime)
+        {
+            batteryLevel -= Time.deltaTime * drainRate;
+            batteryFullImage.fillAmount = (batteryLevel / 100) - 0.1f;
+            Debug.Log(batteryLevel / 100);
+            if(batteryLevel <= 45 && !flashing)
+            {
+                if(batteryLevel > 0)
+                  StartCoroutine(FlashBattery());
+            }
+            if (batteryLevel <= 0)
+            {
+                batteryLevel = 0;
+                drainOverTime = false;
+                ActivateVision();
+            }
+        }
+    }
+  private IEnumerator FlashBattery()
+    {
+        
+        flashing = true;
+        float alpha = 255;
+        while (alpha>=100)
+        {
+            batteryFullImage.color = new Color32(255, 255, 255, (byte)alpha--);
+            
+            Debug.Log(batteryFullImage.color.a);
+            yield return null;
+        }
+        while(alpha < 255)
+        {
+            batteryFullImage.color = new Color32(255, 255, 255, (byte)alpha++);
+
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        flashing = false;
     }
     private void ActivateVision()
     {
+        if (batteryLevel <= 0 && !xrayVolume.enabled) return;
         normalRenderer.SetActive(!normalRenderer.isActive);
         xrayRenderer.SetActive(!xrayRenderer.isActive);
         xrayVolume.enabled = xrayRenderer.isActive;
+        canvas.enabled = xrayVolume.enabled;
+        UIManager.Instance.ChangeCanvasShown();
+
         if (xrayRenderer.isActive)
             StartCoroutine(AnimateVignette());
         foreach (InteractableTaskObject obj in GameObject.FindObjectsOfType<InteractableTaskObject>())
@@ -43,6 +101,7 @@ public class XRayVision : MonoBehaviour
         }
 
     }
+
     private IEnumerator AnimateVignette()
     {
         Vignette vg;
@@ -52,12 +111,12 @@ public class XRayVision : MonoBehaviour
         vg.intensity.value = 1f;
         dof.active = true;
         dof.focusDistance.value = 1;
-      //  vg.center.value = new Vector2(0.5f, -0.1f);
+        //  vg.center.value = new Vector2(0.5f, -0.1f);
 
         while (true)
         {
             vg.intensity.value -= 0.01f;
-           // vg.center.value = new Vector2(0.5f, vg.center.value.y + 0.01f);
+            // vg.center.value = new Vector2(0.5f, vg.center.value.y + 0.01f);
             dof.focusDistance.value += 0.1f;
             if (vg.intensity.value <= 0.3f) break;
             yield return new WaitForSeconds(0.01f);
