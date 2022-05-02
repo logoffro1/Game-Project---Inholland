b8e5a7084d9a1daf6e84150b794c6c4f4b6219f6
egecnr <54850459+egecnr@users.noreply.github.com>
2022-05-02 17:27:37 +0200

    Line generation completed.

--- Assets/MiniGames/WindTurbineMiniGame/Scripts/LineRendererController.cs
+++ Assets/MiniGames/WindTurbineMiniGame/Scripts/LineRendererController.cs
@@ -6,21 +6,61 @@ public class LineRendererController : MonoBehaviour
 {
     // Start is called before the first frame update
     private LineRenderer lr;
-    public Transform[] points;
-
+    private Transform[] points;
+    //Canvas Limits bottom -0.012 x , 0.307 y ,300.009 z,  
+    //Canvas Limits top -0.004 x , -0.347 y ,300.009 z, 
+   
     private void Start()
     {
         lr = GetComponent<LineRenderer>();
+        points = AddRandomPoints(10);
+        Debug.Log($"Length of points {points.Length}");
         lr.positionCount = points.Length;
-        this.points = points;
-    }
-
-    private void Update()
-    {
-        for(int i=0;i<points.Length;i++)
+     
+       foreach(Transform t in points)
+        {
+            Debug.Log($"{t.position.x} x , {t.position.y} y ,{t.position.z} z, ");
+        }
+       
+        
+        lr.sortingOrder = 1;
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.material.color = Color.red;
+        for (int i = 0; i < points.Length; i++)
         {
             lr.SetPosition(i, points[i].position);
         }
     }
 
+    private Transform[] AddRandomPoints(int difficultyLevel)
+    {
+        //Canvas Limits bottom -0.012 x , 0.307 y ,300.009 z  
+        //Canvas Limits top -0.004 x , -0.347 y ,300.009 z
+        //Canvas Limits right side 1.443 x , -0.032 y ,300.009 z 
+        //Canvas Limits left side -1.444 x , -0.057 y ,300.009 z
+        Transform[] points = new Transform[difficultyLevel];
+        float maxLimit = 2.2f;
+        float minLimit = -2.8f;
+        float completeLimit = maxLimit - minLimit;
+        float incrementValue = completeLimit / points.Length;
+        float minY = -0.347f;
+        float maxY = 0.307f;
+        float lastXValue = minLimit;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (i == 0)
+            {
+                GameObject obj1 = new GameObject();
+                obj1.transform.localPosition= new Vector3(minLimit, 0.300f, transform.parent.position.z + 1);
+                points[i] = obj1.transform;
+            }
+            GameObject obj = new GameObject();
+            float xVal = lastXValue + incrementValue;
+            obj.transform.localPosition = new Vector3(lastXValue + incrementValue, Random.Range(minY,maxY), transform.parent.position.z+1);
+            points[i] = obj.transform;
+            lastXValue = xVal;
+        }
+        return points;
+    }
 }
