4fa61ccab1c67f4f092ee97e0d6b8d68e7e34e0c
egecnr <54850459+egecnr@users.noreply.github.com>
2022-05-03 21:05:28 +0200

    Beautiful hintbox is fixed thanks cosmas

--- Assets/MiniGames/WindTurbineMiniGame/Scripts/WindTurbineMinigame.cs
+++ Assets/MiniGames/WindTurbineMiniGame/Scripts/WindTurbineMinigame.cs
@@ -7,28 +7,21 @@ public class WindTurbineMinigame : MiniGameBase
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     public override void CoordinateLevel()
     {
-       
+
     }
     public override void GameFinish(bool succesful)
     {
-
         if (succesful)
         {
             this.GameWon();
-
         }
         else
         {
             this.GameOver();
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -56,7 +56,7 @@ public class MiniGameManager : MonoBehaviour
         UIManager.Instance.ChangeCanvasShown();
         miniGame = Instantiate(miniGamePrefab, new Vector3(0, 0, 300), miniGamePrefab.transform.rotation);
         //Uncomment and implement this for turbine mini game merge!!!!
-        //miniGame.GetComponent<MiniGameBase>().SetLevel();
+        miniGame.GetComponent<MiniGameBase>().SetLevel();
 
         IsPlaying = true;     
     }
