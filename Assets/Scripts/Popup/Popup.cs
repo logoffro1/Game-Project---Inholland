3f78de0b0a30953fafa9287c50ea0209ccc25312
Dewi <dewi.becu@gmail.com>
2022-05-10 10:25:13 +0200

    Popups now have content

--- Assets/Scripts/Popup/Popup.cs
+++ Assets/Scripts/Popup/Popup.cs
@@ -9,9 +9,24 @@ public class Popup : MonoBehaviour
     [HideInInspector] public TaskObjectType Task;
     public bool isMinigamePopup = true; //else is achievement
     public GameObject design;
+
+    public GameObject minigameSewage;
+    public GameObject minigameRecycle;
+    public GameObject minigameSolarPanel;
+    public GameObject minigameStreetLamp;
+    public GameObject minigameWindTurbine;
+    public GameObject minigameTree;
+
+    public TextMeshProUGUI factText;
+
     void Start()
     {
         StartCoroutine(WaitAndMove(1f));
+    }
+
+    private Text GetRandomTextFrom(GameObject container)
+    {
+        return container.transform.GetChild(Random.Range(0, container.transform.childCount)).GetComponent<Text>();
     }
 
     private IEnumerator WaitAndMove(float time)
@@ -19,7 +34,31 @@ public class Popup : MonoBehaviour
         //Wait for minigame
         yield return new WaitForSeconds(3f);
         //change image
-        //set the text
+
+        Text chosenText = null;
+        switch (Task)
+        {
+            case TaskObjectType.Bin:
+                chosenText = GetRandomTextFrom(minigameRecycle);
+                break;
+            case TaskObjectType.StreetLamp:
+                chosenText = GetRandomTextFrom(minigameStreetLamp);
+                break;
+            case TaskObjectType.ManHole:
+                chosenText = GetRandomTextFrom(minigameSewage);
+                break;
+            case TaskObjectType.SolarPanel:
+                chosenText = GetRandomTextFrom(minigameSolarPanel);
+                break;
+            case TaskObjectType.WindTurbine:
+                chosenText = GetRandomTextFrom(minigameWindTurbine);
+                break;
+            case TaskObjectType.Tree:
+                chosenText = GetRandomTextFrom(minigameTree);
+                break;
+        }
+
+        factText.text = chosenText.text;
 
 
         //Go down
@@ -37,27 +76,13 @@ public class Popup : MonoBehaviour
         //Wait for player to read
         yield return new WaitForSeconds(4f);
 
-        //Disappear //doesn't work
-        float alpha = 255f;
-        while (alpha > 1)
+        //Goes back up
+        elapsedTime = 0;
+        while (elapsedTime < time)
         {
-            foreach(Transform child in design.transform)
-            {
-                if (child.TryGetComponent(out Image image))
-                {
-                    Color color = image.color;
-                    color.a--;
-                    child.GetComponent<Image>().color = color;
-                    alpha = color.a;
-                }
-                else
-                {
-                    Color color = child.GetComponent<TextMeshProUGUI>().color;
-                    color.a--;
-                    child.GetComponent<TextMeshProUGUI>().color = color;     
-                }
-            }
-            yield return new WaitForSeconds(0.05f);
+            transform.position = Vector3.Lerp(finalPos, startingPos, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
 
         Destroy(this);
